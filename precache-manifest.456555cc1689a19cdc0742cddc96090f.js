9d6121d781cc715cc439aca2e62b770d186bfd2e
Pavlo Starynskyi <starinskiy@gmail.com>
2021-07-22 12:18:17 +0300

    Updates

---
+++ precache-manifest.456555cc1689a19cdc0742cddc96090f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e627a8427e77c3a7b4048e8edd076b1f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c1f089531e178ffa5ec",
+    "url": "./static/css/main.99e88c86.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1f089531e178ffa5ec",
+    "url": "./static/js/main.8403363a.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
